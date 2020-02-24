89082968c1a0df0f99c349f6ab082dd65fdcc3b7
unknown <jacob.r.hassinger@gmail.com>
2020-02-24 18:26:54 -0500

    made corrections to create table statements, including Primary Keys and writing Foreign Keys into the create table statements.

--- Employees_DB.sql
+++ Employees_DB.sql
@@ -1,63 +1,50 @@
-﻿-- Editted from the QuickDBD export, which was a nice way to get the tables set up for the ERD: https://www.quickdatabasediagrams.com/
+-- Editted from the QuickDBD export, which was a nice way to get the tables set up for the ERD: https://www.quickdatabasediagrams.com/
 
 
-CREATE TABLE "departments" (
-    "dept_no" varchar(10)   NOT NULL,
-    "dept_name" varchar(30)   NOT NULL
+CREATE TABLE departments (
+	dept_no varchar(10) PRIMARY KEY NOT NULL,
+    dept_name varchar(30) NOT NULL
 );
 
-CREATE TABLE "dept_emp" (
-    "emp_no" integer   NOT NULL,
-    "dept_no" varchar(10)   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
+CREATE TABLE employees (
+    emp_no integer PRIMARY KEY NOT NULL,
+    birth_date date NOT NULL,
+    first_name varchar(30) NOT NULL,
+    last_name varchar(50) NOT NULL,
+    gender varchar(1) NOT NULL,
+    hire_date date NOT NULL
 );
 
-CREATE TABLE "dept_manager" (
-    "dept_no" varchar(10)   NOT NULL,
-    "emp_no" integer   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
+CREATE TABLE dept_emp (
+    dept_emp_id serial PRIMARY KEY NOT NULL,
+	emp_no integer REFERENCES employees(emp_no) NOT NULL,
+    dept_no varchar(10) REFERENCES departments(dept_no) NOT NULL,
+    from_date date NOT NULL,
+    to_date date NOT NULL
 );
 
-CREATE TABLE "employees" (
-    "emp_no" integer   NOT NULL,
-    "birth_date" date   NOT NULL,
-    "first_name" varchar(30)   NOT NULL,
-    "last_name" varchar(50)   NOT NULL,
-    "gender" varchar(1)   NOT NULL,
-    "hire_date" date   NOT NULL
+CREATE TABLE dept_manager (
+    dept_manager_id serial PRIMARY KEY NOT NULL,
+	dept_no varchar(10) REFERENCES departments(dept_no) NOT NULL,
+    emp_no integer REFERENCES employees(emp_no) NOT NULL,
+    from_date date NOT NULL,
+    to_date date NOT NULL
 );
 
-CREATE TABLE "salaries" (
-    "emp_no" integer   NOT NULL,
-    "salary" integer   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
+CREATE TABLE salaries (
+    salary_id serial PRIMARY KEY NOT NULL,
+	emp_no integer REFERENCES employees(emp_no) NOT NULL,
+    salary integer NOT NULL,
+    from_date date NOT NULL,
+    to_date date NOT NULL
 );
 
-CREATE TABLE "titles" (
-    "emp_no" integer   NOT NULL,
-    "title" varchar(50)   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
+CREATE TABLE titles (
+    title_id serial PRIMARY KEY NOT NULL,
+	emp_no integer REFERENCES employees(emp_no) NOT NULL,
+    title varchar(50) NOT NULL,
+    from_date date NOT NULL,
+    to_date date NOT NULL
 );
 
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
 
